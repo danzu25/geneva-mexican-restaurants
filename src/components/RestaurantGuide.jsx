706f211e8ea9c0a4db3a71eb699edf9cb27d10c5
danzu25 <danzu25@gmail.com>
2025-01-05 18:10:04 +0100

    Initial project setup with README and main component

---
+++ src/components/RestaurantGuide.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Star, MapPin } from 'lucide-react';
+
+const MexicanRestaurants = () => {
+  // Component code here (the full code we created earlier)
+};
+
+export default MexicanRestaurants;
